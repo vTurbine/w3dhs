4334cfcefea2fecb115ca1673246a63ee86b5403
vTurbine <vtrubiloff@gmail.com>
2016-04-07 16:01:57 +0300

    [+] SoundManager: base data types added

---
+++ src/SoundManager.hs
@@ -0,0 +1,24 @@
+module SoundManager
+    ( Sdmode(..)
+    , Smmode(..)
+    , Sdsmode(..)
+    )
+    where
+
+-- | Sound mode
+data Sdmode     = Sdm_Off
+                | Sdm_PC
+                | Sdm_AdLib
+                deriving (Enum, Show)
+
+-- | Music mode
+data Smmode     = Smm_Off
+                | Smm_AdLib
+                deriving (Enum, Show)
+
+-- | Digi mode
+data Sdsmode    = Sds_Off
+                | Sds_PC
+                | Sds_SoundSource
+                | Sds_SoundBlaster
+                deriving (Enum, Show)
