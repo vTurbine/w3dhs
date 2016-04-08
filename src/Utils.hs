864ecb4a75509758c6af302ab27ad3b95d5390c2
Vladimir Trubilov <vtrubiloff@gmail.com>
2016-04-08 12:38:18 +0000

    [+] Utils: bytesToInt converter added

--- src/Utils.hs
+++ src/Utils.hs
@@ -1,11 +1,17 @@
 module Utils
     ( bytesToString
+    , bytesToInt
     ) where
 
 import Data.Char (chr)
 import Data.Word
 
--- |Converts a list of Word8 into list of chars
+-- |Converts a list of 'Word8' into list of chars
 --
 bytesToString :: [Word8] -> String
 bytesToString = takeWhile (/= '\NUL') . map (chr . fromIntegral)
+
+-- |Converts a list of 'Word8' into 'Int' value
+--
+bytesToInt :: [Word8] -> Int
+bytesToInt xs = foldr (\(v,p) s -> s + (fromIntegral v) * (256 ^ p)) 0 $ zip xs [0..]
