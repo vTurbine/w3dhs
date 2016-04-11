449642c264cb02842440aa0f2074ddcb72d53318
Vladimir Trubilov <vtrubiloff@gmail.com>
2016-04-11 14:32:33 +0000

    [~] Added Huffman decompression
    - Now we can decompress chunks with Huffman

--- src/Resources.hs
+++ src/Resources.hs
@@ -2,14 +2,24 @@ module Resources
     ( loadPalette
     , loadSignOn
     , loadConfig
+    , loadGrResource
     ) where
 
+import qualified Data.ByteString as B
+import qualified Data.Bitstream as BS
+import Data.Binary.Get
 import Data.Word
 import Graphics.UI.SDL
 
 import Resources.Configuration as Config
+import Resources.Dictionary as Huff
+import Resources.Header as Header
 import Resources.OMF as OMF
 import Settings
+import Utils
+
+type BitL = BS.Bitstream BS.Left
+type BitR = BS.Bitstream BS.Right
 
 -- |The 'loadPalette' returns a palette stored into
 -- GAMEPAL.OBJ file. Parsing handled by OMF loader.
@@ -32,3 +42,21 @@ loadSignOn = OMF.findResource "_signon" (gameSrcPath ++ "OBJ/SIGNON.OBJ")
 
 loadConfig :: IO GameConfig
 loadConfig = Config.readConfiguration (gameBinPath ++ "CONFIG" ++ gameBinExt)
+
+
+loadGrResource :: Int -> IO [Word8]
+loadGrResource c = do -- @todo Oh my God, make it lazy..
+    chunk@(ofst, size) <- getChunkOffset c (gameBinPath ++ "VGAHEAD" ++ gameBinExt)
+    d <- B.readFile (gameBinPath ++ "VGAGRAPH" ++ gameBinExt)
+    dict <- Huff.readFile (gameBinPath ++ "VGADICT" ++ gameBinExt)
+
+    let
+        data_seg    = B.drop (fromIntegral ofst) d
+        unp_size    = bytesToInt . B.unpack . B.take 4 $ data_seg
+        comp_data   = B.take size . B.drop 4 $ data_seg
+        unp_data    = Huff.decode dict $ BS.unpack (BS.fromByteString comp_data :: BitL)
+
+    putStrLn $ "Loaded chunk #" ++ show c ++ " " ++ show chunk ++ " -> " ++ show unp_size
+    print $ length unp_data
+
+    return unp_data
