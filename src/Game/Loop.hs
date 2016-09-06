f707113a95a5120d3a48f68144d65e265a63296c
Vladimir Trubilov <vtrubiloff@gmail.com>
2016-09-06 09:44:24 +0300

    [~] `Game/Loop.hs` slightly refactored

--- src/Game/Loop.hs
+++ src/Game/Loop.hs
@@ -1,3 +1,7 @@
+{-
+
+-}
+
 module Game.Loop
     ( gameLoop
     ) where
@@ -9,78 +13,68 @@ import          Graphics.UI.SDL
 import          Data.Word
 
 -- Internal modules import
+import          Game.Agent
 import          Game.Graphics
 import          Game.State
 import          Game.Text
 import          Resources
 
+
+statusLines :: Int
 statusLines = 40
 
 
+-- |
+--
+drawPlayBorder :: GameState -> GameData -> IO ()
 drawPlayBorder gstate gdata = do
 
     let
-        vw = viewWidth gstate
-        vh = viewHeight gstate
-        xl = 160 - vw `div` 2
-        yl = (200 - statusLines - vh) `div` 2
+        vw   = viewWidth gstate
+        vh   = viewHeight gstate
+        xl   = 160 - vw `div` 2
+        yl   = (200 - statusLines - vh) `div` 2
         surf = screen gstate
 
     vwb_Bar surf (Rect 0 0 320 (200 - statusLines)) 127
-
     vwb_Bar surf (Rect xl yl vw vh) 0
 
-    --vwb_Hlin surf (xl - 1) (xl + vw) (yl -  1)   0
-    --vwb_Hlin surf (xl - 1) (xl + vw) (yl + vh) 125
-    --vwb_Vlin surf (yl - 1) (yl + vh) (xl -  1)   0
-    --vwb_Vlin surf (yl - 1) (yl + vh) (xl + vw) 125
+    -- vwb_Hlin surf (xl - 1) (xl + vw) (yl -  1)   0
+    -- vwb_Hlin surf (xl - 1) (xl + vw) (yl + vh) 125
+    -- vwb_Vlin surf (yl - 1) (yl + vh) (xl -  1)   0
+    -- vwb_Vlin surf (yl - 1) (yl + vh) (xl + vw) 125
 
-    --vwb_Plot surf (Point (xl - 1) (yl + vw)) 124
+    -- vwb_Plot surf (Point (xl - 1) (yl + vw)) 124
 
 
-drawPlayerScreen :: Surface -> GameData-> IO ()
-drawPlayerScreen surf gdata = do
+-- |
+--
+drawPlayScreen :: StateT GameState IO ()
+drawPlayScreen = do
+    -- get current game state
+    gstate <- get
 
     let
-        pic = (lumps gdata) !! (86 + (12 - 3)) -- CREDITSPIC
+        gdata = gameData gstate
+        pic   = (lumps gdata) !! (86 + (12 - 3)) -- STATUSBARPIC
 
-    -- FadeOut
+    liftIO $ fadeOut
 
     -- draw the status bar
-    vwb_DrawPic (Point 0 (200 - statusLines)) pic
-{-
+    liftIO $ vwb_DrawPic (Point 0 (200 - statusLines)) pic
 
-    int i,j,p,m;
-    unsigned    temp;
+    drawFace
+    drawHealth
+    drawLives
+    drawLevel
+    drawAmmo
+    drawKeys
+    drawWeapon
+    drawScore
 
-    VW_FadeOut ();
 
-    temp = bufferofs;
-
-    CA_CacheGrChunk (STATUSBARPIC);
-
-    for (i=0;i<3;i++)
-    {
-        bufferofs = screenloc[i];
-        DrawPlayBorder ();
-        VWB_DrawPic (0,200-STATUSLINES,STATUSBARPIC);
-    }
-
-    bufferofs = temp;
-
-    UNCACHEGRCHUNK (STATUSBARPIC);
-
-    DrawFace ();
-    DrawHealth ();
-    DrawLives ();
-    DrawLevel ();
-    DrawAmmo ();
-    DrawKeys ();
-    DrawWeapon ();
-    DrawScore ();
-
--}
-
+-- |
+--
 gameLoop :: StateT GameState IO ()
 gameLoop = do
     -- get current game state
@@ -89,10 +83,7 @@ gameLoop = do
     let
         gdata = gameData gstate
 
-    liftIO $ drawPlayBorder gstate gdata
+    liftIO $ drawPlayBorder gstate gdata    -- @todo this two should have
+    drawPlayScreen                          -- the same signatures
 
-    -- draw the intro screen
-    --liftIO $ drawPlayerScreen (screen gstate) gdata
-
-    -- wait for input here and process to the title screens
     put $ gstate { nextSteps = [GameLoop] }
